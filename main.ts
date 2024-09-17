dbc1a49e7a536ae235f00b670a7e5555e55b6240
nathan-haimanot <nathan.haimanot@mths.ca>
2024-09-17 11:19:54 -0400

    added perimeter and area of rectangle

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
+ * Created by: Nathan 
  * Created on: Sep 2024
  * This program ...
 */
+basic.clearScreen()
+basic.pause(1000)
 
-basic.showString('Hello, Nathan!')
+
+basic.showString('5 + 3 + 5 + 3 =' + (5 + 3 + 5 + 3).toString())
+
+basic.showString('5 * 3 =' + (5 * 3).toString())
